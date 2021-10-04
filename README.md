0f723c0e0717e16c453c5fae3d68ee1cfc2f104e
Emily Dorgan <wmdorgan@gmail.com>
2021-10-03 21:17:45 -0700

    set up local storage, made the past buttons appear with local storage to prevent dupes and wrapped everything in a documnet.ready jquery function; also added some screenshots

--- README.md
+++ README.md
@@ -1,15 +1,10 @@
 # Weather Dashboard
 
-This project uses server-side APIs to get the latest up-to-date weather in any city around the world. The user can search for a city and see the current temperature, humidity, wind speed and UV index along with a 5 day forecast. The previous 8 searches show up in the sidebar 
-
-## To Do list
-
-* Make the button with event listener that takes data and sets it to searchedCity variable
-* Make it so that a new button is generated in the search history for the past 8 cities searched
+This project uses server-side APIs to get the latest up-to-date weather in any city around the world. The user can search for a city and see the current temperature, humidity, wind speed and UV index along with a 5 day forecast. This search along with previous searches show up in the sidebar. These past searches persist between page reload (max 15).
 
 ## Getting Started
 
-
+Enter a VALID city name to get the weather in that city
 
 ## Deployed Link
 
@@ -17,6 +12,9 @@ This project uses server-side APIs to get the latest up-to-date weather in any c
 
 ## Screenshots
 
+![Before any search results](screenshots/pre-search.png)
+![User searches for Berkeley](screenshots/berkeley.png)
+![UV index's color changes based on intensity](screenshots/uv.png)
 
 
 ## Built With
@@ -25,6 +23,7 @@ This project uses server-side APIs to get the latest up-to-date weather in any c
 * [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)
 * [Javascript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
 * [Bootstrap](https://getbootstrap.com/)
+* [jQuery](https://jquery.com/)
 
 
 ## Authors
--- assets/js/script.js
+++ assets/js/script.js
@@ -12,80 +12,92 @@
 
 // When user clicks on the generated button, set searchedCity to the value of the generated button and repeat step 3
 
-var searchedCity;
-var lat;
-var lon;
-var today = moment().format('(M/D/YYYY)');
+$(document).ready(function(){       // waits to the document to fully load before doing any jQuery
 
-var requestUrlGeo= "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&appid=266918c8637e87badd2e272562101ade";
+    var searchedCity;           // Global variable for the searched city
+    var lat;                    // it's lattitute   
+    var lon;                    // and it's longitute
+    var today = moment().format('(M/D/YYYY)');  // today returns the exact date in (MM/D/YYYY) format
 
-function getWeather(city){
-    $.ajax({                                    
-        url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=266918c8637e87badd2e272562101ade",
-        method: 'GET',
-    }).then(function (response) {
-        lat = response.coord.lat;
-        lon = response.coord.lon;
-        // console.log(lat, lon);
-        var requestUrlWeather= "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=266918c8637e87badd2e272562101ade&units=imperial"; // using one-call-API since it gives a 7 day forecast
-        $.ajax({
-            url: requestUrlWeather,
+    function loadButtons(){                          // Function that loads the previous search buttons from local storage when called
+        var pastCities = Object.keys(localStorage);     // declares an array, pastCities, of all keys in local storage
+        for(i=0; i < pastCities.length; i++){           // Iteraters through the local storage keys
+            if(i<14){                                   // caps the maximum at 14 past search results
+                var newBtn = $('<button>').text(pastCities[i]);     //creates a new button with the text is the name of the searched city
+                newBtn.attr('value', pastCities[i]);                    // sets the value to be used in an event listener
+                newBtn.attr('class', 'btn btn-outline-secondary m-1');  // sets bootstrap css
+                $('#search-results').append(newBtn);                    // appends the new button
+            }
+        }
+
+    }
+
+    function getWeather(city){                                  // main calling function
+        $.ajax({                                    
+            url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=266918c8637e87badd2e272562101ade",
             method: 'GET',
-        }).then(function (data){
-            console.log(data);
-            var iconUrl = "http://openweathermap.org/img/wn/"+ data.current.weather[0].icon +"@2x.png";
-            var weatherIcon = $('<img>').attr('src', iconUrl);
-            $('#city-name').text(searchedCity + " " + today);
-            $('#city-name').append(weatherIcon);
-            $('#temp').html("Temperature: " + data.current.temp + " &#176;F");
-            $('#humidity').text("Humidity: " + data.current.humidity + "%");
-            $('#wSpeed').text(" Wind Speed: " + data.current.wind_speed + " MPH");
-            $('#UV').text(data.current.uvi);
-            $('#UV').removeAttr('class');
-            if(data.current.uvi < 3){
-                $('#UV').addClass('favorable rounded custom-display');
-            }
-            else if(data.current.uvi >= 3 && data.current.uvi < 6){
-                $('#UV').addClass('low rounded custom-display');
-            }
-            else if(data.current.uvi >= 6 && data.current.uvi < 8){
-                $('#UV').addClass('moderate rounded custom-display');
-            }
-            else if(data.current.uvi >= 8 && data.current.uvi < 11){
-                $('#UV').addClass('severe rounded custom-display');
-            }
-            else if(data.current.uvi >= 11){
-                $('#UV').addClass('extreme rounded custom-display');
-            }
-            $('#fiveDay').empty();                  // empties out previous 5-day forecast from previous searches
-            for(var i=0; i<5; i++){                // I'm using a template literal to make a whole bunch of HTML at once to generate the 5 day forecast with a for loop
-                $('#fiveDay').append(`
-                <div class="col-2" style="width: 14rem;">
-                    <div class="card bg-info p-2 text-white">
-                        <p>${moment().add(i+1, 'days').format('(M/D/YYYY)')}</p>
-                        <img src="${"http://openweathermap.org/img/wn/"+ data.daily[i].weather[0].icon +"@2x.png"}">
-                        <p>Temp: ${data.daily[i].temp.day} &#176;F</p>
-                        <p>Humidity: ${data.daily[i].humidity}%</p>
+        }).then(function (response) {
+            lat = response.coord.lat;
+            lon = response.coord.lon;
+            // console.log(lat, lon);
+            var requestUrlWeather= "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=266918c8637e87badd2e272562101ade&units=imperial"; // using one-call-API since it gives a 7 day forecast
+            $.ajax({
+                url: requestUrlWeather,
+                method: 'GET',
+            }).then(function (data){
+                var iconUrl = "http://openweathermap.org/img/wn/"+ data.current.weather[0].icon +"@2x.png";
+                var weatherIcon = $('<img>').attr('src', iconUrl);
+                $('#city-name').text(searchedCity + " " + today);
+                $('#city-name').append(weatherIcon);
+                $('#temp').html("Temperature: " + data.current.temp + " &#176;F");
+                $('#humidity').text("Humidity: " + data.current.humidity + "%");
+                $('#wSpeed').text(" Wind Speed: " + data.current.wind_speed + " MPH");
+                $('#UV').text(data.current.uvi);
+                $('#UV').removeAttr('class');
+                if(data.current.uvi < 3){
+                    $('#UV').addClass('favorable rounded custom-display');
+                }
+                else if(data.current.uvi >= 3 && data.current.uvi < 6){
+                    $('#UV').addClass('low rounded custom-display');
+                }
+                else if(data.current.uvi >= 6 && data.current.uvi < 8){
+                    $('#UV').addClass('moderate rounded custom-display');
+                }
+                else if(data.current.uvi >= 8 && data.current.uvi < 11){
+                    $('#UV').addClass('severe rounded custom-display');
+                }
+                else if(data.current.uvi >= 11){
+                    $('#UV').addClass('extreme rounded custom-display');
+                }
+                $('#fiveDay').empty();                  // empties out previous 5-day forecast from previous searches
+                for(var i=0; i<5; i++){                // I'm using a template literal to make a whole bunch of HTML at once to generate the 5 day forecast with a for loop
+                    $('#fiveDay').append(`
+                    <div class="col-2" style="width: 14rem;">
+                        <div class="card bg-info p-2 text-white">
+                            <p>${moment().add(i+1, 'days').format('(M/D/YYYY)')}</p>
+                            <img src="${"http://openweathermap.org/img/wn/"+ data.daily[i].weather[0].icon +"@2x.png"}">
+                            <p>Temp: ${data.daily[i].temp.day} &#176;F</p>
+                            <p>Humidity: ${data.daily[i].humidity}%</p>
+                        </div>
                     </div>
-                </div>
-                `)
-            }
-        });
-    });   
-};
+                    `)
+                }
+                localStorage.setItem(searchedCity, searchedCity);
+                $('#search-results').empty();
+                loadButtons();
+            });
+        });   
+    };
 
-$('#search-results').on('click', '.btn-outline-secondary', function(event){
-    console.log("did I get here");
-    searchedCity = $(this).val();
-    getWeather(searchedCity);
-});
+    $('#search-results').on('click', '.btn-outline-secondary', function(event){
+        searchedCity = $(this).val();
+        getWeather(searchedCity);
+    });
 
-$('#searchBtn').on('click', function(event){
-    event.preventDefault();
-    searchedCity = $(this).siblings('#cityName').val()
-    getWeather(searchedCity);
-    var newBtn = $('<button>').text(searchedCity);
-    newBtn.attr('value', searchedCity);
-    newBtn.attr('class', 'btn btn-outline-secondary m-1');
-    $('#search-results').append(newBtn);
+    $('#searchBtn').on('click', function(event){
+        event.preventDefault();
+        searchedCity = $(this).siblings('#cityName').val()
+        getWeather(searchedCity);
+    });
+    loadButtons();                  // calls the 'loadButtons' function when the browser first loads
 });
